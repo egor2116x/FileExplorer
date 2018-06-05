2df4dc1cc3e3701e07c5c252de2f6fa64f703913
egor2116x <egor2116x@gmail.com>
2018-06-05 16:30:22 +0300

    Refactoring of source code

---
+++ FileExplorer/FileExplorerEngine.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include "FileExplorerEngine.h"
+
+size_t FileExplorerEngine::GetFileSize(WIN32_FIND_DATA & _wfd)
+{
+    return (_wfd.nFileSizeHigh * (MAXDWORD + 1)) + _wfd.nFileSizeLow;
+}
+
+void FileExplorerEngine::BuildNewFolderPath(std::wstring & folderPath, const std::wstring & newFolderName)
+{
+    folderPath = folderPath.substr(0, folderPath.size() - 1);
+    folderPath += newFolderName;
+    folderPath += L"\\*";
+}
+
+void FileExplorerEngine::GetListFilesInFolder(std::wstring & folderPath)
+{
+    if (folderPath.empty())
+    {
+        if (m_rootFolderPath.empty())
+        {
+            throw UserInputException(INCORRECT_USER_INPUT, "No such file root directory");
+        }
+        folderPath = m_rootFolderPath;
+    }
+
+    WIN32_FIND_DATA wfd;
+    HANDLE hFile = INVALID_HANDLE_VALUE;
+
+    hFile = FindFirstFile(folderPath.c_str(), &wfd);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        return;
+    }
+
+    do
+    {
+
+        if (IsDirectory(wfd.dwFileAttributes) && std::wstring(wfd.cFileName)[0] != L'.')
+        {
+            ++m_level;
+            g_fileMap.push_back(Item(m_level, DIR_MARK, folderPath.substr(0, folderPath.size() - 1), std::wstring(wfd.cFileName), this->GetFileSize(wfd), hFile));
+            BuildNewFolderPath(folderPath, std::wstring(wfd.cFileName));
+            GetListFilesInFolder(folderPath);
+            FilePathOperation::CutLastFilePathItem(folderPath);
+            FilePathOperation::CutLastFilePathItem(folderPath);
+            folderPath += L"\\*";
+            --m_level;
+        }
+        else if (std::wstring(wfd.cFileName)[0] != L'.')
+        {
+            g_fileMap.push_back(Item(m_level, FILE_MARK, folderPath.substr(0, folderPath.size() - 1), std::wstring(wfd.cFileName), this->GetFileSize(wfd), hFile));
+        }
+        else
+        {
+            g_fileMap.push_back(Item(m_level, DIR_MARK, folderPath.substr(0, folderPath.size() - 1), std::wstring(wfd.cFileName), this->GetFileSize(wfd), hFile));
+        }
+
+    } while (FindNextFile(hFile, &wfd) != 0);
+
+    FindClose(hFile);
+    return;
+}
--- FileExplorer/FileExplorerEngine.h
+++ FileExplorer/FileExplorerEngine.h
@@ -24,57 +24,6 @@ private:
 	std::wstring m_rootFolderPath;
 };
 
-
-void FileExplorerEngine::GetListFilesInFolder(std::wstring & folderPath)
-{
-	if (folderPath.empty())
-	{
-		if (m_rootFolderPath.empty())
-		{
-			throw UserInputException(INCORRECT_USER_INPUT, "No such file root directory");
-		}
-		folderPath = m_rootFolderPath;
-	}
-
-	WIN32_FIND_DATA wfd;
-	HANDLE hFile = INVALID_HANDLE_VALUE;
-
-	hFile = FindFirstFile(folderPath.c_str(), &wfd);
-
-	if (hFile == INVALID_HANDLE_VALUE)
-	{
-		return;
-	}
-
-	do
-	{
-
-		if (IsDirectory(wfd.dwFileAttributes) && std::wstring(wfd.cFileName)[0] != L'.')
-		{
-			++m_level;
-			g_fileMap.push_back(Item(m_level, DIR_MARK, folderPath.substr(0, folderPath.size() - 1), std::wstring(wfd.cFileName), this->GetFileSize(wfd), hFile));
-			BuildNewFolderPath(folderPath, std::wstring(wfd.cFileName));
-			GetListFilesInFolder(folderPath);
-			FilePathOperation::CutLastFilePathItem(folderPath);
-			FilePathOperation::CutLastFilePathItem(folderPath);
-			folderPath += L"\\*";
-			--m_level;
-		}
-		else if (std::wstring(wfd.cFileName)[0] != L'.')
-		{
-			g_fileMap.push_back(Item(m_level, FILE_MARK, folderPath.substr(0, folderPath.size() - 1), std::wstring(wfd.cFileName), this->GetFileSize(wfd), hFile));
-		}
-		else
-		{
-			g_fileMap.push_back(Item(m_level, DIR_MARK, folderPath.substr(0, folderPath.size() - 1), std::wstring(wfd.cFileName), this->GetFileSize(wfd), hFile));
-		}
-
-	} while (FindNextFile(hFile, &wfd) != 0);
-
-	FindClose(hFile);
-	return;
-}
-
 template<typename Output_Type>
 void FileExplorerEngine::print(Output_Type & out)
 {
@@ -97,20 +46,9 @@ void FileExplorerEngine::print(Output_Type & out)
 	}
 }
 
-void FileExplorerEngine::BuildNewFolderPath(std::wstring & folderPath, const std::wstring & newFolderName)
-{
-	folderPath = folderPath.substr(0, folderPath.size() - 1);
-	folderPath += newFolderName;
-	folderPath += L"\\*";
-}
-
 template<typename Action>
 void FileExplorerEngine::makeAction(Action & action)
 {
 	std::for_each(g_fileMap.begin(), g_fileMap.end(), action);
 }
 
-size_t FileExplorerEngine::GetFileSize(WIN32_FIND_DATA & _wfd)
-{
-	return (_wfd.nFileSizeHigh * (MAXDWORD + 1)) + _wfd.nFileSizeLow;
-}
